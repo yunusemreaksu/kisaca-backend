0c0d327ebcce1da5bee0be2621d6f04591335834
Yunus Emre Aksu <emre_aksu@live.com>
2022-05-26 17:35:34 +0300

    Added .env file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const app = express();
+const PORT = process.env.KISACA_APP_API_URL || 8080
 
 app.use("/users", (req, res, next) => {
   res.send("<p>Test</p>");
 });
 
-app.listen(8080);
+app.listen(PORT);

976d87390ea0d8f2c588f8e2e762fbbe5617bd2f
Yunus Emre Aksu <emre_aksu@live.com>
2022-05-27 14:36:39 +0300

    Added body-parser package

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.KISACA_APP_API_URL || 8080
 
-app.use("/users", (req, res, next) => {
+app.use("/main", (req, res, next) => {
   res.send("<p>Test</p>");
 });
 
-app.listen(PORT);
+app.listen(8080);

d94bef94e84acf1371acea1d164fdd82431ffb9f
Yunus Emre Aksu <emre_aksu@live.com>
2022-05-31 10:22:41 +0300

    Added main controller

---
+++ controllers/main-controller.js
@@ -0,0 +1,27 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_NEWS = [
+  {
+    id: "n1",
+    date: "27.05.2022 15:01",
+    text: "Lorem ipsum",
+  },
+  {
+    id: "n2",
+    date: "27.05.2022 16:00",
+    text: "Lorem ipsum dolor sit amet",
+  },
+];
+
+const getNewsById = (req, res, next) => {
+  const newsId = req.params.nid; //nid: news id
+  const news = DUMMY_NEWS.find((n) => {
+    return n.id === newsId;
+  });
+  if (!news) {
+    return next(new HttpError("Sayfa görüntülenemedi.", 404));
+  }
+  res.json({ news: news });
+};
+
+exports.getNewsById = getNewsById;
--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,32 +1,9 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const mainController = require("../controllers/main-controller");
 
 const router = express.Router();
 
-const DUMMY_NEWS = [
-  {
-    id: "n1",
-    date: "27.05.2022 15:01",
-    text: "Lorem ipsum",
-  },
-  {
-    id: "n2",
-    date: "27.05.2022 16:00",
-    text: "Lorem ipsum dolor sit amet",
-  },
-];
-
-//nid: news id
-router.get("/:nid", (req, res, next) => {
-  const newsId = req.params.nid;
-  const news = DUMMY_NEWS.find((n) => {
-    return n.id === newsId;
-  });
-  if (!news) {
-    return next(new HttpError("Sayfa görüntülenemedi.", 404));
-  }
-  res.json({ news: news });
-});
+router.get("/:nid", mainController.getNewsById);
 
 module.exports = router;

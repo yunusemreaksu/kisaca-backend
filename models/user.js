29086cadd9c71c26897d625a6ce359fff28061c1
Yunus Emre Aksu <emre_aksu@live.com>
2022-06-08 12:20:29 +0300

    Fixed some typos

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { tyoe: String, required: true, unique: true }, // unique creates index for email, speeds up the queuing process.
+  email: { type: String, required: true, unique: true }, // unique creates index for email, speeds up the queuing process.
   password: { type: String, required: true, minlength: 6 },
   comments: { type: String, required: true },
 });
